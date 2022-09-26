d945a328503d3a23aa693e7f5377b7c9de3324f4
unknown <miguel.semedo.2@outlook.com>
2022-09-26 20:05:01 +0100

    Fetch Data / Define enviroment variables

---
+++ scripts.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+// console.log(process.env);
+
+// Unsplash API
+const count = 10;
+const apiKey = process.env.API_KEY;
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get photos from Unsplash API
+async function getPhotos() {
+	try {
+		const response = await fetch(apiUrl);
+		const data = await response.json();
+		console.log(data);
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+// On Load
+getPhotos();
