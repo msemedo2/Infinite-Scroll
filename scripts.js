9a2af1bec82c6c29791aa77eae0a0e32c601effa
unknown <miguel.semedo.2@outlook.com>
2022-09-27 09:59:55 +0100

    Create Image Elements

--- scripts.js
+++ scripts.js
@@ -1,17 +1,40 @@
-require('dotenv').config();
-// console.log(process.env);
+const imageContainer = document.getElementById('img-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
 
 // Unsplash API
 const count = 10;
-const apiKey = process.env.API_KEY;
+const apiKey = 'HaKfvTX7HV5WdlCyc0YaBvmPpaFwkDIDRVx3zYdzwqc';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Create Elements for Links & Photos, Add to DOM
+function displayPhotos() {
+	// Run function for each object in photosArray
+	photosArray.forEach((photo) => {
+		// Create <a> to link to Unsplash
+		const item = document.createElement('a');
+		item.setAttribute('href', photo.links.html);
+		item.setAttribute('target', '_blank');
+
+		// Create <img> for photo
+		const img = document.createElement('img');
+		img.setAttribute('src', photo.urls.regular);
+		img.setAttribute('alt', photo.alt_description);
+		img.setAttribute('title', photo.alt_description);
+
+		// Put <img> <a>, then put both inside image Container
+		item.appendChild(img);
+		imageContainer.appendChild(item);
+	});
+}
 
 // Get photos from Unsplash API
 async function getPhotos() {
 	try {
 		const response = await fetch(apiUrl);
-		const data = await response.json();
-		console.log(data);
+		photosArray = await response.json();
+		displayPhotos();
 	} catch (err) {
 		console.log(err);
 	}
